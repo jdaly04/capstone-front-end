822614baaf5f680c347488d7ad6c7a3219903971
jdaly04 <julia.m.daly@gmail.com>
2017-01-14 14:18:14 -0500

    Adds functioning front-end auth
    Auth works on front-end and back-end now.
    Still need to remove sign-up button since admin-only auth will be end
    product. Updated the config file to show local host as apiOrigin.host
    url. Added the auth directory and api.js, events.js, ui.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,9 +2,13 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
+const authEvents = require('./auth/events.js');
+//const crudEvents = require('./crud/events.js');
 
 $(() => {
   setAPIOrigin(location, config);
+  authEvents.addHandlers();
+  //crudEvents.getAllHandler();
 });
 
 // use require with a reference to bundle the file and use it in this file
