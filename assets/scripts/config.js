8b7f1db4111008bb2946db34e3a14df63d91a6d4
jdaly04 <julia.m.daly@gmail.com>
2017-01-14 19:46:29 -0500

    Updates config file to remove last "/" from api.
    Change heroku deployed back-end url to remove last "/"

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     // production: 'https://ga-wdi-boston.herokuapp.com',
-     production: 'https://fathomless-brook-52618.herokuapp.com/'
+     production: 'https://fathomless-brook-52618.herokuapp.com'
     // production: 'https://localhost:4741'
   },
 };
