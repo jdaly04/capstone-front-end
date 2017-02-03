a08bb0ca69101416ed9cf9ff07a6e163df6529fb
jdaly04 <julia.m.daly@gmail.com>
2017-02-03 16:47:35 -0500

    Fixes create not working issue
    Attempt at fixing the fact that "create" only works part of the time.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const store = require('../store');
+const events = require('../crud/events.js');
 
 
 const success = (data) => {
@@ -8,14 +9,14 @@ const success = (data) => {
   $('.messages').empty();
 };
 
-const signUpSuccess = (data) => {
-  store.token = data.user.token;
-  $('#signUpModal').modal("hide");
-  $('#sign-up').trigger("reset");
-  $('.messages').text('Signed up! Now sign in!');
-};
+// const signUpSuccess = (data) => {
+//   store.token = data.user.token;
+//   $('#signUpModal').modal("hide");
+//   $('#sign-up').trigger("reset");
+//   $('.messages').text('Signed up! Now sign in!');
+// };
 
-const signInSuccess = (data) => { //if you have curly braces you can have more than one expression. without, only 1 expression allowed.
+const signInSuccess = (data) => {
   store.user = data.user;
   $('#ch-password').show();
   $('#sign-out-button').show();
@@ -25,26 +26,8 @@ const signInSuccess = (data) => { //if you have curly braces you can have more t
   $('#patch').show();
   $('#new-prod').show();
   $('#create-product').show();
-  // $('#update').show();
-  // $('#updateSubmit').show();
-  // $('#delete').hide();
-  // $('#delete-button').hide();
-  // $('#update-submit').hide();
-  // $('#updateLibName').hide();
-  // $('#create-form').show();
-  // $('#get-products').show();
-  // $('.list-of-fundraisers').show();
-  // $('.get-all-products').show();
   $('#sign-in').trigger("reset");
-  $('.messages').text('Success! You can now create a new product and/or view your current products! Soon, you can add fundraisers to products!(You can still view all the current fundraisers!)');
-
-
-
-
-  //  button for "My Product" should show up. when you click "my product, you should
-  // see a table of your saved fundraisers. Then, you should be able to browse all,
-  // and update your product by adding a new one, or be able to delete one from your
-  // product.
+  $('#new-prod').on('click', events.onCreateNewButton);
 };
 
 const changePasswordSuccess = (data) => {
@@ -64,14 +47,6 @@ const signOutSuccess = () => {
   $('.user-messages').hide();
   $('#new-prod').hide();
   $('#create-product').hide();
-  // $('#update-submit').hide();
-  // $('#updateLibName').hide();
-  // $('#create-form').hide();
-  // $('#get-products').hide();
-  // $('.get-all-products').hide();
-  // $('.get-all-products').empty();
-  // $('.list-of-fundraisers').empty();
-  // $('.messages').empty();
 };
 
 const failure = (error) => {
@@ -83,7 +58,7 @@ module.exports = {
   failure,
   success,
   signInSuccess,
-  signUpSuccess,
+  // signUpSuccess,
   changePasswordSuccess,
   signOutSuccess,
 };
--- assets/scripts/crud/events.js
+++ assets/scripts/crud/events.js
@@ -8,6 +8,7 @@ const store = require('../store.js');
 //const store = require('../store.js');
 
 const onCreateNewButton = function(event) {
+  console.log("in onCreateNewButton");
   event.preventDefault();
   $('#create-modal').modal('show');
 };
@@ -16,7 +17,6 @@ const onCreateProduct = function(event) {
   event.preventDefault();
   let data = getFormFields(event.target);
   data.product.user_id = store.user.id;
-  console.log("inside onCreateProduct data is", data);
   api.createProduct(data)
     .then(ui.createProductSuccess)
     // .then(function() {
@@ -42,7 +42,6 @@ const onUpdateProduct = function(event) {
   event.preventDefault();
   let data = getFormFields(event.target);
   // data.product.user_id = store.user.id;
-  console.log("in onUpdateProduct data is", data);
   api.updateProduct(data)
     .then(ui.updateProductSuccess)
     .catch(ui.updateFailure);
@@ -57,19 +56,20 @@ const onViewProducts = function(event) {
       $('#create-form').off();
       $('.updateProduct').off();
       $('.deleteProduct').off();
-      $('#new-prod').on('click', onCreateNewButton);
       $('#create-form').on('submit', onCreateProduct);
       //$('.edit-product').on('click', onUpdateProductButton);
       $('.updateProduct').on('submit', onUpdateProduct);
       $('.deleteProduct').on('submit', onDeleteProduct);
 
-    })  //getAllSuccess needs to be updated
+    })
     .catch(ui.failure);
   // }
 };
 //
     const getAllHandler = () => {
       $('#view-all').on('click', onViewProducts);
+      $('#new-prod').on('click', onCreateNewButton);
+
     };
 
 module.exports = {
